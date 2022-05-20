4433c10912e574b63e7c64d14226ee4e900fdd9e
mboostrom <maxboostrom@gmail.com>
2022-05-19 22:07:59 -0400

    added styling to profile page

--- client/src/components/GolfRoundTile.js
+++ client/src/components/GolfRoundTile.js
@@ -3,12 +3,28 @@ import React from "react"
 const GolfRoundTile = (props) => {
   const course = props.courses.find(({ id }) => id === props.round.courseId)
 
+  const dateTime = new Date(props.round.createdAt)
+  const date = dateTime.toLocaleDateString()
+
   return (
-    <div>
-      <h2>{props.round.score}</h2>
-      <p>{props.round.holesPlayed}</p>
-      <p>{course.name}</p>
-      <hr></hr>
+    <div className="golf-round-tile">
+      <p className="date">{date}</p>
+      <div className="tile-body">
+        <div className="tile-score">
+          <div className="score-top">
+            <h4>Score:</h4>
+          </div>
+          <div className="score-box">
+            <h2>{props.round.score}</h2>
+          </div>
+        </div>
+        <div className="tile-info">
+          <div className="holes-played">
+            <p >Holes Played: {props.round.holesPlayed}</p>
+          </div>
+          <p>{course.name}</p>
+        </div>
+      </div>
     </div>
   )
 }
--- server/src/serializers/GolfRoundSerializer.js
+++ server/src/serializers/GolfRoundSerializer.js
@@ -1,7 +1,7 @@
 class ReviewsSerializer {
   static async getSummary(round) {
     try {
-      const allowedAttributes = ["id", "holesPlayed", "score", 'courseId', 'userId']
+      const allowedAttributes = ["id", "holesPlayed", "score", 'courseId', 'userId', 'createdAt']
 
       let serializedGolfRound = {}
       for (const attribute of allowedAttributes) {
